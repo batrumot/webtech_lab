6dc818a6f102bb9806edd1d400d6e99b27242d3a
batrumot <hhhntt@gmail.com>
2018-03-21 16:35:40 +0700

    fdusfgsdjfhdsk

---
+++ upload.php
@@ -0,0 +1,9 @@
+<html>
+	<head><title>upload file</title></head>
+	<body>
+		<form action="new.php" method="POST" enctype="multipart/form-data">
+			<input type="file" name="file1">
+			<input type="submit" value="submit">
+		</form>
+	</body>
+</html>
